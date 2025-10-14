fc830e6b23cb2a31306ebf7b958c31e344987048
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2025-10-15 11:00:01 +1300

    feat: added alert and avatar ui components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,6 @@
+export * from './components/alert';
+export * from './components/alert-dialog';
+export * from './components/avatar';
 export * from './components/button';
 export * from './components/card';
 export * from './components/panel-label';
