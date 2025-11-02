be36be44ebb08c60ecf8b1c12aecc7a40d3c7ffd
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2025-11-03 04:42:13 +1300

    feat: added initial astro site

---
+++ packages/demo/prettier.config.cjs
@@ -0,0 +1,19 @@
+/**
+ * Prettier config for demo package with Astro support
+ */
+module.exports = {
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss'),
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+
