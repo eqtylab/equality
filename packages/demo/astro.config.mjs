d78faa639fd0fafb840cef1eaa0716f121c52aee
Giuliana Aliotti <125221024+giuliana-gladeye@users.noreply.github.com>
2025-11-05 08:14:08 +1300

    Feature/formatting imports and type fixes (#6)

--- packages/demo/astro.config.mjs
+++ packages/demo/astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import { resolve } from "path";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import mdx from "@astrojs/mdx";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+        "@/components": resolve(__dirname, "./src/components"),
+        "@/lib": resolve(__dirname, "./src/lib"),
+        "@/layouts": resolve(__dirname, "./src/layouts"),
+        "@/pages": resolve(__dirname, "./src/pages"),
+        "@/styles": resolve(__dirname, "./src/styles"),
+      },
+    },
+  },
+  integrations: [mdx()],
 });
