01f1e380fee6e8304379310adb71b8c5c0725d19
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2025-11-11 10:42:08 +1300

    feat: added alert dialog, alert and avatar component pages

---
+++ packages/demo/src/components/demo/avatar.tsx
@@ -0,0 +1,20 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@eqtylab/equality";
+
+export const AvatarDemo = ({
+  variant = "default",
+}: {
+  variant?: "default" | "fallback";
+}) => {
+  return (
+    <Avatar>
+      <AvatarImage
+        src={
+          variant === "default"
+            ? "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            : undefined
+        }
+      />
+      <AvatarFallback>RG</AvatarFallback>
+    </Avatar>
+  );
+};
