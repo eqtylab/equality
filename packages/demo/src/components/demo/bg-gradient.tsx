201d006fbca08d59e91f340d02328b92b543dcfd
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2025-11-17 11:31:54 +1300

    feat: added placement variants to bg gradient demo

--- packages/demo/src/components/demo/bg-gradient.tsx
+++ packages/demo/src/components/demo/bg-gradient.tsx
@@ -4,12 +4,14 @@ import tokens from "../../../../tokens/equality-tokens.json";
 
 export const BgGradientDemo = ({
   variant = "primary",
+  placement = "full",
 }: {
   variant?: keyof typeof tokens.Light.color.brand;
+  placement?: "full" | "top";
 }) => {
   return (
     <div className="relative h-150 w-full *:size-full">
-      <BgGradient theme={variant} />
+      <BgGradient theme={variant} placement={placement} />
     </div>
   );
 };
