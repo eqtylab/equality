89a95213cb442450e3f135cbd5411d152790b1ed
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2025-11-20 10:22:31 +1300

    feat: updated spinner component

--- packages/ui/src/components/spinner/spinner.tsx
+++ packages/ui/src/components/spinner/spinner.tsx
@@ -2,22 +2,15 @@ import styles from '@/components/spinner/spinner.module.css';
 import { cn } from '@/lib/utils';
 
 interface SpinnerProps {
-  size?: number;
-  color?: string;
-  borderWidth?: number;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  color?: 'theme' | 'primary' | 'secondary' | 'green' | 'red' | 'gold';
 }
 
-// TODO: Optimize to take variants for size - Color should always be dark/light
-export const Spinner = ({
-  size = 40,
-  color = 'border-gray-400',
-  borderWidth = 4,
-}: SpinnerProps) => {
-  const spinnerStyle = {
-    width: `${size}px`,
-    height: `${size}px`,
-    borderWidth: `${borderWidth}px`,
-  };
-
-  return <div className={cn(styles['spinner'], color)} style={spinnerStyle} />;
+export const Spinner = ({ size = 'md', color = 'theme' }: SpinnerProps) => {
+  return (
+    <div className={styles['spinner-container']}>
+      <div className={cn(styles['spinner'], styles[size], styles[color])} />
+      <div className={cn(styles['spinner-background'], styles[size])} />
+    </div>
+  );
 };
