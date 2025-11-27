832cba2e23987cbdafffbc25f32351acfc13ed07
Kate Scherz <kate.scherz@gladeye.com>
2025-11-27 17:06:28 +1300

    Use icons instead of text for theme toggle

--- packages/demo/src/components/ui/theme-toggle.tsx
+++ packages/demo/src/components/ui/theme-toggle.tsx
@@ -1,18 +1,20 @@
 import { theme } from "@/stores/themeStore";
-import { Switch } from "@eqtylab/equality";
+import { Icon, Switch } from "@eqtylab/equality";
 import { useStore } from "@nanostores/react";
+import { Sun, Moon } from "lucide-react";
 
 export const ThemeToggle = () => {
   const $theme = useStore(theme);
 
   return (
     <div className="flex items-center gap-2">
+      <Icon icon={<Sun />} aria-label="Light" />
       <Switch
         checked={$theme === "dark"}
         size="sm"
         onCheckedChange={(checked) => theme.set(checked ? "dark" : "light")}
       />
-      Theme
+      <Icon icon={<Moon />} aria-label="Dark" />
     </div>
   );
 };
