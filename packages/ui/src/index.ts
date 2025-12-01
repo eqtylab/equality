fd27bb34007ee3d2e3271a7465471d48fc1cbf9f
Kate Scherz <kate.scherz@gladeye.com>
2025-12-01 13:38:50 +1300

    Add missing portal export

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,6 +3,7 @@ export * from './components';
 
 /* THEME */
 export * from './theme/theme-provider';
+export * from './theme/portal';
 export * from './theme/tokens.css';
 export * from './theme/theme.css';
 
