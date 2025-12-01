0a84eb5cc73dfec0ca520e1973fe8ecd899c36ca
Kate Scherz <kate.scherz@gladeye.com>
2025-12-01 15:15:14 +1300

    Delete demo theme provider as no longer needed, as can react to changes within the store

--- packages/demo/src/stores/theme-store.ts
+++ packages/demo/src/stores/theme-store.ts
@@ -8,3 +8,14 @@ export const $theme = persistentAtom<Theme>(
   "theme",
   (getInitialTheme() as Theme | undefined) ?? DEFAULT_THEME,
 );
+
+// Update data-equality-theme attribute on the root element
+$theme.listen((value) => {
+  const themeProviderRootElement = document.querySelector(
+    "[data-equality-theme]",
+  );
+  if (themeProviderRootElement) {
+    themeProviderRootElement.setAttribute("data-equality-theme", value);
+    document.documentElement.style.colorScheme = value;
+  }
+});
