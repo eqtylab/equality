6441a75a2be52f1467055a3fc4fd0c9beee93bd8
Kate Scherz <kate.scherz@gladeye.com>
2025-12-17 13:58:21 +1300

    feat: Add language as a badge to code block

--- packages/ui/src/components/code-block/code-block.tsx
+++ packages/ui/src/components/code-block/code-block.tsx
@@ -2,6 +2,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import darkStyle from 'react-syntax-highlighter/dist/esm/styles/prism/a11y-dark';
 import lightStyle from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 
+import { Badge } from '@/components/badge/badge';
 import styles from '@/components/code-block/code-block.module.css';
 import { CopyButton } from '@/components/copy-button/copy-button';
 import { cn } from '@/lib/utils';
@@ -9,7 +10,7 @@ import { useTheme } from '@/theme/hooks/use-theme';
 
 interface CodeBlockProps {
   className?: string;
-  title: string;
+  title?: string;
   code: string;
   language?: string;
   variant?: 'primary' | 'success' | 'danger' | 'warning';
@@ -33,8 +34,15 @@ const CodeBlock = ({
   return (
     <div className={cn(styles['code-block'], className)}>
       <div className={styles.header}>
-        <span className={styles.title}>{title}</span>
-        <CopyButton value={copy || code} size="sm" />
+        <div className={styles['header-left']}>
+          {title && <span className={styles.title}>{title}</span>}
+          <Badge variant="neutral" className={styles['badge']}>
+            {language}
+          </Badge>
+        </div>
+        <div className={styles['header-right']}>
+          <CopyButton value={copy || code} size="sm" />
+        </div>
       </div>
       <div className={cn(styles.content, styles[variant])}>
         <div className={cn(styles['scroll-container'], 'styled-vertical-scrollbar')}>
