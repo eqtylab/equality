e8758bed6a49205eed1ef823a2592fb2b502a484
kate-gladeye <59935556+kate-gladeye@users.noreply.github.com>
2025-12-18 14:31:58 +1300

    Feature/codeblock highlighting (#33)
    Co-authored-by: Henry Wilkinson <henry@wilkinson.graphics>

--- packages/ui/src/components/code-block/code-block.tsx
+++ packages/ui/src/components/code-block/code-block.tsx
@@ -1,34 +1,62 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import darkStyle from 'react-syntax-highlighter/dist/esm/styles/prism/a11y-dark';
+import lightStyle from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
+
+import { Badge } from '@/components/badge/badge';
 import styles from '@/components/code-block/code-block.module.css';
 import { CopyButton } from '@/components/copy-button/copy-button';
 import { cn } from '@/lib/utils';
+import { useTheme } from '@/theme/hooks/use-theme';
 
 interface CodeBlockProps {
   className?: string;
-  title: string;
+  title?: string;
   code: string;
-  variant?: 'primary' | 'success' | 'danger' | 'warning';
+  language?: string;
+  variant?: 'neutral' | 'primary' | 'success' | 'danger' | 'warning';
   codeLabel?: string;
   copy?: string;
 }
+
+const WRAP = true;
 
 const CodeBlock = ({
   className,
   title,
   code,
-  variant = 'primary',
+  language = 'text',
+  variant = 'neutral',
   codeLabel,
   copy,
 }: CodeBlockProps) => {
+  const [theme] = useTheme();
+
   return (
     <div className={cn(styles['code-block'], className)}>
       <div className={styles.header}>
-        <span className={styles.title}>{title}</span>
-        <CopyButton value={copy || code} size="sm" />
+        <div className={styles['header-left']}>
+          {title && <span className={styles.title}>{title}</span>}
+          <Badge variant="neutral" className={styles['badge']}>
+            {language}
+          </Badge>
+        </div>
+        <div className={styles['header-right']}>
+          <CopyButton value={copy || code} size="sm" />
+        </div>
       </div>
       <div className={cn(styles.content, styles[variant])}>
         <div className={cn(styles['scroll-container'], 'styled-vertical-scrollbar')}>
           {codeLabel && <div className={styles['code-label']}>{codeLabel}</div>}
-          <pre className={styles.code}>{code}</pre>
+          <SyntaxHighlighter
+            language={language}
+            style={theme === 'dark' ? darkStyle : lightStyle}
+            wrapLines={WRAP}
+            wrapLongLines={WRAP}
+            className={styles.pre}
+            codeTagProps={{ className: cn(styles.code, { [styles.wrap]: WRAP }) }}
+          >
+            {code}
+          </SyntaxHighlighter>
         </div>
       </div>
     </div>
