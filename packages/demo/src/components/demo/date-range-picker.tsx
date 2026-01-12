1eb5c3e4c820d772d8144ad8a559bf5a2ee37485
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2026-01-13 11:15:21 +1300

    feat: added minimun width to date range picker component

--- packages/demo/src/components/demo/date-range-picker.tsx
+++ packages/demo/src/components/demo/date-range-picker.tsx
@@ -1,8 +1,20 @@
 import { DateRangePicker } from "@eqtylab/equality";
 import { useState } from "react";
 
-export const DateRangePickerDemo = () => {
+export const DateRangePickerDemo = ({
+  displayMinWidth = false,
+}: {
+  displayMinWidth?: boolean;
+}) => {
   const [dateRange, setDateRange] = useState<{ from?: Date; to?: Date }>({});
+
+  if (displayMinWidth) {
+    return (
+      <div className="flex">
+        <DateRangePicker dateRange={dateRange} onSelect={setDateRange} />
+      </div>
+    );
+  }
 
   return <DateRangePicker dateRange={dateRange} onSelect={setDateRange} />;
 };
