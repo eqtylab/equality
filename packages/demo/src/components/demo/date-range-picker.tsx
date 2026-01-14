7e9ce277c0fdf08a6d3a1489924a20edf846d9ec
Henry Wilkinson <henry@wilkinson.graphics>
2026-01-14 00:02:56 -0500

    Revert "feat: added minimun width to date range picker component"
    This reverts commit 1eb5c3e4c820d772d8144ad8a559bf5a2ee37485.

--- packages/demo/src/components/demo/date-range-picker.tsx
+++ packages/demo/src/components/demo/date-range-picker.tsx
@@ -1,20 +1,8 @@
 import { DateRangePicker } from "@eqtylab/equality";
 import { useState } from "react";
 
-export const DateRangePickerDemo = ({
-  displayMinWidth = false,
-}: {
-  displayMinWidth?: boolean;
-}) => {
+export const DateRangePickerDemo = () => {
   const [dateRange, setDateRange] = useState<{ from?: Date; to?: Date }>({});
-
-  if (displayMinWidth) {
-    return (
-      <div className="flex">
-        <DateRangePicker dateRange={dateRange} onSelect={setDateRange} />
-      </div>
-    );
-  }
 
   return <DateRangePicker dateRange={dateRange} onSelect={setDateRange} />;
 };
