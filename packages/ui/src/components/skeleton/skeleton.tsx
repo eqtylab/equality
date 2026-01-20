c598a845c040bd7d2db29104202b257717495e5f
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2026-01-20 16:47:24 +1300

    feat: added elevation to skeleton

--- packages/ui/src/components/skeleton/skeleton.tsx
+++ packages/ui/src/components/skeleton/skeleton.tsx
@@ -1,8 +1,20 @@
 import styles from '@/components/skeleton/skeleton.module.css';
+import { Elevation, ELEVATION, generateElevationVariants } from '@/lib/elevations';
 import { cn } from '@/lib/utils';
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn(styles['skeleton'], className)} {...props} />;
+const skeletonElevationVariants = generateElevationVariants(styles, 'skeleton', ELEVATION.RAISED);
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  elevation?: Elevation;
+}
+
+function Skeleton({ className, elevation = ELEVATION.RAISED, ...props }: SkeletonProps) {
+  return (
+    <div
+      className={cn(styles['skeleton'], skeletonElevationVariants({ elevation }), className)}
+      {...props}
+    />
+  );
 }
 
 export { Skeleton };
