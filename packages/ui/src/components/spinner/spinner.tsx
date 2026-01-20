941dd3c53bc267cf1fbcbe5eeea365260abf46e1
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2026-01-20 16:47:42 +1300

    feat: added elevation to spinner

--- packages/ui/src/components/spinner/spinner.tsx
+++ packages/ui/src/components/spinner/spinner.tsx
@@ -1,16 +1,34 @@
 import styles from '@/components/spinner/spinner.module.css';
+import { Elevation, ELEVATION, generateElevationVariants } from '@/lib/elevations';
 import { cn } from '@/lib/utils';
+
+const spinnerElevationVariants = generateElevationVariants(
+  styles,
+  'spinner-background',
+  ELEVATION.RAISED
+);
 
 interface SpinnerProps {
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   variant?: 'neutral' | 'primary' | 'success' | 'danger' | 'warning';
+  elevation?: Elevation;
 }
 
-export const Spinner = ({ size = 'md', variant = 'neutral' }: SpinnerProps) => {
+export const Spinner = ({
+  size = 'md',
+  variant = 'neutral',
+  elevation = ELEVATION.RAISED,
+}: SpinnerProps) => {
   return (
     <div className={styles['spinner-container']}>
       <div className={cn(styles['spinner'], styles[size], styles[variant])} />
-      <div className={cn(styles['spinner-background'], styles[size])} />
+      <div
+        className={cn(
+          styles['spinner-background'],
+          styles[size],
+          spinnerElevationVariants({ elevation })
+        )}
+      />
     </div>
   );
 };
