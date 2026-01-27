79b47384b235115eb8bce55ebacbbdb59db95fe4
Tyler <tyler@eqtylab.io>
2026-01-27 10:57:51 -0700

    nix support

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+            pkgs.just
+            pkgs.nodejs
+            pkgs.yarn
+            pkgs.nodePackages.pnpm
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      }
+    );
+}
