c91234894d16739df745a8d682b1e8398eec8b0c
giuliana-gladeye <giuliana.aliotti@gladeye.com>
2026-07-06 12:42:50 +1200

    feat: updated command docs

--- packages/demo/src/components/demo/command.tsx
+++ packages/demo/src/components/demo/command.tsx
@@ -1,29 +1,33 @@
 import {
   Command,
   CommandInput,
+  CommandList,
   CommandEmpty,
   CommandGroup,
   CommandItem,
 } from "@eqtylab/equality";
 import { Check } from "lucide-react";
+import { useState } from "react";
+
+const options = ["Option 1", "Option 2", "Option 3"];
 
 export const CommandDemo = () => {
+  const [selected, setSelected] = useState("Option 1");
+
   return (
     <Command>
       <CommandInput placeholder="Search repositories..." />
-      <CommandEmpty>No repositories found.</CommandEmpty>
-      <CommandGroup heading="Repositories">
-        <CommandItem value="Option 1" onSelect={() => {}}>
-          Option 1
-          <Check />
-        </CommandItem>
-        <CommandItem value="Option 2" onSelect={() => {}}>
-          Option 2
-        </CommandItem>
-        <CommandItem value="Option 3" onSelect={() => {}}>
-          Option 3
-        </CommandItem>
-      </CommandGroup>
+      <CommandList>
+        <CommandEmpty>No repositories found.</CommandEmpty>
+        <CommandGroup heading="Repositories">
+          {options.map((option) => (
+            <CommandItem key={option} value={option} onSelect={setSelected}>
+              {option}
+              {selected === option && <Check />}
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
     </Command>
   );
 };
